36c08d390dcfa28878bbc2c094c9a9fb36cffe10
Ulrich GIRAUD <ulrich.giraud@evaneos.com>
2025-04-23 11:39:48 +0200

    Initialize the provider

--- apis/v1beta1/register.go
+++ apis/v1beta1/register.go
@@ -13,7 +13,7 @@ import (
 
 // Package type metadata.
 const (
-	Group   = "template.upbound.io"
+	Group   = "cloudamqp.evaneos.com"
 	Version = "v1beta1"
 )
 
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/upbound/upjet-provider-template
+module github.com/evaneos/provider-cloudamqp
 
 go 1.21
 
