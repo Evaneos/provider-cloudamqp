697ec9d2606fb44913a5122b4e0c851e091c1099
Ulrich GIRAUD <ulrich.giraud@evaneos.com>
2025-04-25 15:06:23 +0200

    feat(connectionDetails): add additional connection details to vpc and instance

--- config/vpc/config.go
+++ config/vpc/config.go
@@ -1,12 +1,34 @@
 package vpc
 
-import "github.com/crossplane/upjet/pkg/config"
+import (
+	"fmt"
+	"strings"
+
+	// "github.com/crossplane/crossplane-runtime/pkg/logging"
+	"github.com/crossplane/upjet/pkg/config"
+	// "sigs.k8s.io/controller-runtime/pkg/log/zap"
+)
 
 // Configure configures individual resources by adding custom ResourceConfigurators.
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("cloudamqp_vpc", func(r *config.Resource) {
 		r.ShortGroup = "cloudamqp"
+
+		// zl := zap.New(zap.UseDevMode(true))
+		// logr := logging.NewLogrLogger(zl.WithName("provider-gcp"))
+		r.Sensitive.AdditionalConnectionDetailsFn = func(attr map[string]any) (map[string][]byte, error) {
+			conn := map[string][]byte{}
+
+			// If we are in GCP, register the vpc id
+			if region, ok := attr["region"].(string); ok && strings.HasPrefix(region, "google-compute-engine") {
+				if a, ok := attr["vpc_name"].(string); ok {
+					conn["gcp_id"] = []byte(fmt.Sprintf("projects/cloudamqp/global/networks/%s", a))
+				}
+			}
+			return conn, nil
+		}
 	})
+
 	p.AddResourceConfigurator("cloudamqp_vpc_peering", func(r *config.Resource) {
 		r.ShortGroup = "cloudamqp"
 		r.Kind = "VpcPeering"
